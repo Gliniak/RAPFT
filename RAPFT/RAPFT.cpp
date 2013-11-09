95fd67aac566d156219a246df3087c29c4ca2bb7
Radosław Gliński <Gliniak93@gmail.com>
2013-11-09 19:30:17 +0100

    [008] Added Basic Support For Copy-Run Mode
    ToDo:
     - Copying all .bps files
     - Copying All Startup Arguments To new Process
     - Window Hide
     - (Optional/Impossibru) Make "CREATE_PROTECTED_PROCESS" Flag Usable

--- RAPFT/RAPFT.cpp
+++ RAPFT/RAPFT.cpp
@@ -16,13 +16,37 @@ unsigned int CalculateTimeToHalfHour()
 	return at + calculated*60-actualTime->tm_sec;
 }
 
-//It's Going To Be Useful Soon
-std::string ExePath() 
+std::string GetPath() 
 {
     char buffer[MAX_PATH];
-    GetModuleFileNameA( NULL, buffer, MAX_PATH );
-    std::string::size_type pos = std::string( buffer ).find_last_of( "\\/" );
-    return std::string( buffer ).substr( 0, pos + 1);
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+    std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+    return std::string(buffer).substr(0, pos + 1);
+}
+
+std::string GetExeName() 
+{
+    char buffer[MAX_PATH];
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+    std::string::size_type pos = std::string(buffer).find_last_of( "\\/" );
+	return std::string(buffer).substr(pos + 1, std::string(buffer).length() - pos);
+}
+
+std::string CopyToLocation(std::string _location)
+{
+	std::string path;
+	path.append(GetPath().c_str());
+	path.append(GetExeName().c_str());
+
+	_location.append(GetExeName().c_str());
+
+	if(CopyFileA(path.c_str(), LPCSTR(_location.c_str()), FALSE) == 0)
+		printf("COPYING FILE STATUS: FAILED! ERROR CODE: %u \n", GetLastError());
+	else 
+	{
+		printf("Copying Exe To New Location FINISHED. NEW LOCATION: %s\n", _location.c_str());
+		return _location;
+	}
 }
 
 int main(int argc, char* argv[])
@@ -38,10 +62,25 @@ int main(int argc, char* argv[])
 			printf("'-f' - Read Theme Data Based On File\n");
 			printf("'-t' - Time in seconds till next play\n");
 			printf("'-a' - Add Application To Autostart (UNIMPLEMENTED) \n");
-			printf("'-c' - Create Copy Of Application And Run It (UNIMPLEMENTED) \n");
+			printf("'-c' - Create Copy Of Application And Run It \n");
 			printf("'-p' - DO YOU WANT TO PLAY A GAME?! \n");
 			printf("------------- HELP -------------\n");
 			system("pause");
+			return 0;
+		}
+
+		if(strcmp(argv[i], "-c") == 0)
+		{
+			std::string asd = CopyToLocation(argv[i+1]);
+
+			PROCESS_INFORMATION pi = {0};
+			STARTUPINFOA         si = {0};
+
+			si.cb = sizeof(STARTUPINFOA);
+
+			CreateProcessA(asd.c_str(), NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+			CloseHandle(pi.hProcess);
+			CloseHandle(pi.hThread);
 			return 0;
 		}
 
@@ -59,7 +98,7 @@ int main(int argc, char* argv[])
 	while(1)
 	{
 		int _STTP = floor(difftime(_TTP, time(NULL)));
-		printf("REMAIN TIME: %i SECONDS \n", _STTP);
+		printf("I'M A' FIRIN' MAH LAZER IN: %i SECONDS \n", _STTP);
 
 		if(_STTP <= 0)
 		{
